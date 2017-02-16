ad6c0dbdcf2a3c41227c48c12aae5acd460e5670
c0demark <kumarvijay336@gmail.com>
2017-02-16 22:52:34 +0530

    added a static web page and custom Express route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+app.use('/express-status', function(req, res, next) {
+  res.json({ running: true });
+});
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
